001fb9d5f4b3c5831a97eef223d365d73b58a7fc
Rohit <sketchlearn7@gmail.com>
2020-02-17 18:29:08 +0530

    first commit

---
+++ BankAccount/Model/AccountOwner.swift
@@ -0,0 +1,13 @@
+//
+//  AccountOwner.swift
+//  BankAccount
+//
+//  Created by MACBOOK on 17/02/20.
+//  Copyright © 2020 Saini. All rights reserved.
+//
+
+import Foundation
+
+class AccountOwner: NSObject{
+    init?(firstname: String,)
+}
---
+++ BankAccount/Model/LastNameValidator.swift
@@ -0,0 +1,9 @@
+//
+//  LastNameValidator.swift
+//  BankAccount
+//
+//  Created by MACBOOK on 17/02/20.
+//  Copyright © 2020 Saini. All rights reserved.
+//
+
+import Foundation
